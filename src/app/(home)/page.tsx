19b8e319da6597a20a692685c8c43634e7a36c96
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2024-04-04 11:01:00 +0900

    fetch client render

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,14 +1,29 @@
+"use client"
 import Image from "next/image";
 import {Metadata} from "next";
+import {useEffect, useState} from "react";
 
-export const metadata: Metadata = {
-    title: "Home",
-    description: "best movies",
-};
-export default function Home() {
+
+// export const metadata: Metadata = {
+//     title: "Home",
+//     description: "best movies",
+// };
+export default function Page() {
+    const [isLoading, setLoading] = useState(true)
+    const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies")
+        const json = await response.json();
+        setMovies(json)
+        setLoading(false)
+
+    }
+    useEffect(() => {
+        getMovies()
+    }, [])
   return (
     <div>
-        <h1>hello</h1>
+        {isLoading? "Loading...":JSON.stringify(movies)}
     </div>
   );
 }
